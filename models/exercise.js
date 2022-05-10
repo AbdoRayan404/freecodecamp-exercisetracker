fadc9b2a9fae9bb2b7f97ca375aac8a9b9c5f5cf
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-10 07:58:24 +0200

    Schema models: (user, exercise)

---
+++ models/exercise.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    ownerId:{ //FK
+      type: String,
+      required: true
+    },
+    description:{
+      type: String,
+      require: true
+    },
+    duration: {
+      type: Number,
+      require: true,
+      min: [1, 'Duration too high'],
+    },
+    date:{
+      type: Date,
+    }
+  })
+
+module.exports = exerciseSchema.model('exercise', exerciseSchema)
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    _id:{ //PK
+      type: String
+    },
+    username:{
+      type: String,
+      unique: true,
+      required: true
+    }
+  })
+
+module.exports = userSchema.model('user', userSchema)
