1df3873731282f73b46e547aaa65ea821494eac6
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-10 08:40:19 +0200

    API route: POST '/api/users' creating new user

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const { v4: uuidv4 } = require('uuid')
 const mongoose = require('mongoose');
-const { stringify } = require('uuid');
 
 const app = express()
+const User = require('./models/user')
+const Exercise = require('./models/exercise')
+
 
 require('dotenv').config()
 app.use(cors())
@@ -18,7 +20,34 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//////////////////////
+//    API routes    //
+//////////////////////
 
+app.post('/api/users', (req, res)=>{
+  
+  //check if username is provided
+  if(!req.body.username){
+    res.json({'error':'Please provide a username'})
+    return;
+  }
+
+  let newUUID = uuidv4();
+
+  //creating user document
+  const newUser = new User({
+    _id: newUUID,
+    username: req.body.username
+  })
+
+  //saving the user document
+  newUser.save((err,data)=>{
+    if(err) console.log(err);
+    if(data){
+      res.json({username:req.body.username, _id:newUUID})
+    }
+  })
+})
 
 
 
