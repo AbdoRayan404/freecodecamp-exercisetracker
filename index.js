ef9a3b258544bddf0a239e20017692c3eaacd28e
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-10 07:41:59 +0200

    setting up the server file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
+const mongoose = require('mongoose');
+const { stringify } = require('uuid');
 
+const app = express()
+
+require('dotenv').config()
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.connect(process.env.MONGO_URI)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
